f8272cfa8a1e1966ade809629200b6ad044761f2
DevRanjo <rania.leontiou@outlook.com>
2026-04-14 11:09:21 +0200

    comments added

--- LABB1/Task2.c
+++ LABB1/Task2.c
@@ -19,35 +19,47 @@ void convertFloat(representation* rep, float flt){
     float frac = flt - whole_num;           //the fraction part of the float number
 
     //convert whole number part to binary 
+    char whole_binary[16] = {0};
+    int index = 0; 
+
+    if(whole_num == 0){
+        whole_binary[index++] = '0';        
+    }
+    else{
+        while(whole_num > 0){
+            whole_binary[index++] = (whole_num % 2);  //add each (remainder as) binary number to the array
+            whole_num = (int) whole_num / 2;           //use next even number
+        }
+        //after going to the end number -> reverse to get the right binary order
+        int last_index = index - 1;  //last index
+
+        for(int j=0; j < last_index/2; j++){            //go through half iterations to switch elements correctly in the array
+            char temp = whole_binary[j];                
+            whole_binary[j] = whole_binary[last_index - j]; //switch later elements with prior order in array
+            whole_binary[last_index - j] = temp;            
+        }
+    }
 
     //convert fraction number part to binary
 
-    //combine both binary parts
+    char fraction_binary[16] = {0};
+    int index = 0; 
+
+
+    //combine both binary parts with snprintf
 
     //find exponent
 
     //extract mantissa
-
-    //convert mantissa string to binary 
+    //typ ta 1 minus på de fyra första byten ^^
 
     //combine everything sign + exponent + mantissa
 
-    int* exponent = malloc(3);
-    float* mantissa = malloc(4);
-
-    *exponent = 0; 
-
-    while(flt != 1){
-        flt/2;
-        ++exponent; 
-        mantissa = flt; 
-    }
-    *mantissa = flt - 2^((float*)exponent);
 }
 
 
 int main(){
-    representation* number_representation = malloc(8);
+    representation* rep = malloc(8);
 
 
     return 1; 
