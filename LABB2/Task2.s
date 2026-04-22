d33c5c02fbfd1ebac9262adbce8bc02d38ade928
Malin Gelin <malin.gelin2@gmail.com>
2026-04-22 16:25:27 +0200

    updates in task2.s

--- LABB2/Task2.s
+++ LABB2/Task2.s
@@ -8,8 +8,11 @@ value: .word 0xBD5B7DDE @ skapar hex variabel
 main:
     push {ip, lr}
 
-    ldr r0, = value @ hämta addressen som value ligger i minnet
+    ldr r0, =value @ hämta addressen som value ligger i minnet
     ldr r0, [r0]   @ gå till addressen och hämta värdet
+
+    asr r1, r0, #1  @hoppa över sign med att skifta ett steg åt höger
+    mov r0, r1
 
     bl int_out         @ hoppa till int_out funktionen
 
