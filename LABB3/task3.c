46fb99c720432746ead6323c325b445b6064b707
DevRanjo <rania.leontiou@outlook.com>
2026-05-06 14:17:23 +0200

    working first task

--- LABB3/task3.c
+++ LABB3/task3.c
@@ -53,10 +53,56 @@ int main(){
 
     Node* L1 = create_node(0, 'A');
     append_to_list(L1, create_node(1, 'B'));
-    append_to_list(L1, create_node(1, 'C'));
-    
+    append_to_list(L1, create_node(2, 'C'));
+    append_to_list(L1, create_node(3, 'D'));
+    append_to_list(L1, create_node(1, 'E'));
+    append_to_list(L1, create_node(2, 'F'));
+    append_to_list(L1, create_node(3, 'G'));
+    append_to_list(L1, create_node(1, 'H'));
+    append_to_list(L1, create_node(2, 'I'));
+    append_to_list(L1, create_node(3, 'J'));
+
     print_list(L1);
-    
+
+    Node* tmp = L1;
+    int list_int_size = 0;
+    int list_char_size = 0;
+    int list_struct_size = 0;
+    int list_pointer_size = 0; 
+    int only_fields_size = 0;
+
+    if(tmp!= NULL){
+        list_struct_size += sizeof(*tmp); //size of struct Node
+
+        list_int_size += sizeof(tmp->val);
+        list_char_size += sizeof(tmp->c);
+        list_pointer_size += sizeof(tmp->next);
+    }
+    only_fields_size = list_int_size + list_char_size + list_pointer_size;
+    printf("\nChecking one struct Node:\n");
+    printf("Only size of struct Node: %d\n", list_struct_size);
+    printf("Sum of all fields in struct node: %d\n", only_fields_size);
+
+    //skip first to continue with rest of list 
+    tmp = tmp->next;
+    while(tmp!=NULL){
+        //total size of all struct nodes in list
+        list_struct_size += sizeof(*tmp);
+
+        //total sizes of the seperate fields. 
+        list_int_size += sizeof(tmp->val);
+        list_char_size += sizeof(tmp->c);
+        list_pointer_size += sizeof(tmp->next);
+
+        tmp = tmp->next;
+    }
+    only_fields_size = list_int_size + list_char_size + list_pointer_size;
+
+    printf("\nChecking 10 element list with size:\n");
+    printf("Struct size list: %d\n", list_struct_size);
+    printf("Only all fields in list size: %d", only_fields_size);
+
+    free(tmp);
     free_list(L1);
     return 1; 
 }
